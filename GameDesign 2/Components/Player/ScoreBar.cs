acb3d60abe77968abab2fbe91fbcf4c0d7e7c80f
R de Gier <robin.de.gier@hva.nl>
2013-11-11 13:38:10 +0100

    Addition to ScoreBar for better functionality.. 	+ No more position and halfsize in the constructor. From now on it's made through a private static function. 	+ GDComp's Origin now is has a value. 	+ Added Comment.

--- GameDesign 2/Components/Player/ScoreBar.cs
+++ GameDesign 2/Components/Player/ScoreBar.cs
@@ -4,14 +4,28 @@ using System.Linq;
 using System.Text;
 using GameDesign_2.Screens;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace GameDesign_2.Components.Player
 {
+    /// <summary>
+    /// The ScoreBar used during gameplay. This bar keeps track of the current score and time.
+    /// The gained or lossed score adds at X% per second. 
+    /// The players looses score after a certain amount of time.
+    /// Score subtraction has priority over score addition.
+    /// </summary>
     public class ScoreBar : GDComp
     {
         //Drop the score by 5% after 10 seconds.
         private const int SecondsPerPointDrop = 10;
         private const int PercentDropByTimeBorder = 5;
+
+        //Bar colors.
+        private const Color Background = Color.Black;
+        private const Color PointLoss = new Color(new Vector3(150, 0, 0));      //Red.
+        private const Color PointGain = new Color(new Vector3(0, 150, 0));      //Green.
+        private const Color PointBalance = new Color(new Vector3(0, 0, 150));   //Blue.
 
         private readonly int PercentPerSecond;
 
@@ -21,9 +35,17 @@ namespace GameDesign_2.Components.Player
         private int toSubtract;
         private int goalScore;
         private float timeCounter;
-
-        public ScoreBar(Game1 game, Vector2 position, Vector2 halfSize, int goalScore)
-            : base(game, Shape.None, position, halfSize)
+        private float timeLeft;
+        private Texture2D texture;
+        private bool isTimeLimited = false;
+        
+        /// <summary>
+        /// Basic ScoreBar with no time limit.
+        /// </summary>
+        /// <param name="game">The current running game.</param>
+        /// <param name="goalScore">The level's goal score.</param>
+        public ScoreBar(Game1 game, int goalScore)
+            : base(game, Shape.None, SetPosition(game), SetHalfSize(game))
         {
             toAdd = 0;
             toSubtract = 0;
@@ -32,6 +54,38 @@ namespace GameDesign_2.Components.Player
             Score = 0;
         }
 
+        /// <summary>
+        /// A ScoreBar with a possible timeLimit. If timeLimit is 0, then there's no timelimit.
+        /// </summary>
+        /// <param name="game">The current running game.</param>
+        /// <param name="goalScore">The level's goal score.</param>
+        /// <param name="timeLimit">The level's timelimit (0 means no limit).</param>
+        public ScoreBar(Game1 game, int goalScore, float timeLimit)
+            : this(game, goalScore)
+        {
+            isTimeLimited = timeLimit > 0;
+
+            if (isTimeLimited)
+            {
+                timeLeft = timeLimit;
+            }
+        }
+
+        protected override void LoadContent()
+        {
+            ContentManager content = GDGame.GetActiveScreen().Content;
+            texture = content.Load<Texture2D>("square");
+
+            Origin = new Vector2(texture.Width * 0.5f, texture.Height * 0.5f);
+
+            base.LoadContent();
+        }
+
+        /// <summary>
+        /// Adds score over time. The amount is added to a pile which slowly becomes less.
+        /// Score subtraction has priority over addition.
+        /// </summary>
+        /// <param name="amount">Amount of score the player has earned.</param>
         public void AddScore(int amount)
         {
             if (amount < 0)
@@ -42,6 +96,11 @@ namespace GameDesign_2.Components.Player
             toAdd += amount;
         }
 
+        /// <summary>
+        /// Subtracts score over time. The amount is added to a pile which slowly becomes less.
+        /// Score subtraction has priority over addition.
+        /// </summary>
+        /// <param name="amount">Amount of score the player has lossed.</param>
         public void SubtractScore(int amount)
         {
             if (amount < 0)
@@ -50,6 +109,34 @@ namespace GameDesign_2.Components.Player
             }
 
             toSubtract += amount;
+        }
+
+        /// <summary>
+        /// Sets this bar's halfsize.
+        /// </summary>
+        /// <param name="game">The running game.</param>
+        /// <returns></returns>
+        private static Vector2 SetHalfSize(Game1 game)
+        {
+            Viewport vp = game.GraphicsDevice.Viewport;
+            float halfWidth = vp.Width * 0.5f * 0.8f;
+            float halfHeight = vp.Height * 19 / 20;
+
+            return new Vector2(halfWidth, halfHeight);
+        }
+
+        /// <summary>
+        /// Sets this bar's position.
+        /// </summary>
+        /// <param name="game">The running game.</param>
+        /// <returns></returns>
+        private static Vector2 SetPosition(Game1 game)
+        {
+            Viewport vp = game.GraphicsDevice.Viewport;
+            float y = vp.Height * 9 / 10;
+            float x = vp.Width * 0.5f;
+
+            return new Vector2(x, y);
         }
 
         public override void Update(GameTime gameTime)
@@ -77,6 +164,7 @@ namespace GameDesign_2.Components.Player
                 toAdd -= gain;
             }
 
+            //Check if reahed any score border.
             if (Score <= 0)
             {
                 (GDGame.GetActiveScreen() as GameplayScreen).GameOver();
@@ -86,7 +174,7 @@ namespace GameDesign_2.Components.Player
                 (GDGame.GetActiveScreen() as GameplayScreen).Won();
             }
 
-            //Lastly the timer.
+            //Lastly the time.
             timeCounter += dt;
             if (timeCounter >= SecondsPerPointDrop)
             {
@@ -94,7 +182,23 @@ namespace GameDesign_2.Components.Player
                 SubtractScore((int)(PercentDropByTimeBorder * 0.01f * goalScore));
             }
 
+            if (isTimeLimited)
+            {
+                timeLeft -= dt;
+                if (timeLeft <= 0)
+                {
+                    (GDGame.GetActiveScreen() as GameplayScreen).GameOver();
+                }
+            }
+
             base.Update(gameTime);
+        }
+
+        public override void Unload()
+        {
+            texture.Dispose();
+
+            base.Unload();
         }
     }
 }
