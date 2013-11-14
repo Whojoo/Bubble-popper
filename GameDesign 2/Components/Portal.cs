6f67b400f7689195013892dc2594251cc9993e40
R de Gier <robin.de.gier@hva.nl>
2013-11-14 14:38:21 +0100

    Added Portal and Spawner..
    Both classes are still empty for now.

---
+++ GameDesign 2/Components/Portal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace GameDesign_2.Components
+{
+    public class Portal : GDComp
+    {
+        public Portal(Game1 game, Vector2 position)
+            : base(game, Shape.None, position, new Vector2())
+        {
+
+        }
+    }
+}
