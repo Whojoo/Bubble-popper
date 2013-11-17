4731b6119c5f00dd7a6ae57eb12cb127795665e7
R de Gier <robin.de.gier@hva.nl>
2013-11-17 16:48:52 +0100

    SpawnPortal can now be drawn.

--- GameDesign 2/Components/SpawnPortal.cs
+++ GameDesign 2/Components/SpawnPortal.cs
@@ -3,18 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace GameDesign_2.Components
 {
     public class SpawnPortal : GDComp
     {
+        private const float PNGRadius = 100;
+
         private Random randy;
+        private Vector2 scaleCorrection;
+        private Texture2D texture;
 
         public SpawnPortal(Game1 game, Vector2 position, Vector2 halfSize)
             : base(game, Shape.None, position, halfSize)
         {
             //Randomly pressed numbers for seed.
             randy = new Random(5936);
+            scaleCorrection = new Vector2(
+                halfSize.X * 2 / PNGRadius,
+                HalfSize.Y * 2 / PNGRadius);
+        }
+
+        protected override void LoadContent()
+        {
+            texture = GDGame.GetActiveScreen().Content.
+                Load<Texture2D>("circle");
+            Origin = new Vector2(texture.Width * 0.5f, texture.Height * 0.5f);
+
+            base.LoadContent();
         }
 
         /// <summary>
@@ -31,6 +48,13 @@ namespace GameDesign_2.Components
             yPos += ((float)randy.NextDouble() - 0.5f) * 2 * HalfSize.Y;
 
             return new Vector2(xPos, yPos);
+        }
+
+        public override void Draw(GameTime gameTime, SpriteBatch batch)
+        {
+            batch.Draw(texture, Position, null, Color.Purple, 0, Origin, scaleCorrection, SpriteEffects.None, 0);
+
+            base.Draw(gameTime, batch);
         }
     }
 }
