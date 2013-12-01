13f3f5b314570d2240c70cfdced6b2e6f81e9f7f
Robin de Gier <de.gier.robin@gmail.com>
2013-12-01 19:58:07 +0100

    Added StateMachine logic to BossStateMachine.

--- GameDesign 2/States/StateMachines/BossStateMachine.cs
+++ GameDesign 2/States/StateMachines/BossStateMachine.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using GameDesign_2.Screens;
+using GameDesign_2.States.GameStates;
+using Microsoft.Xna.Framework;
 
 namespace GameDesign_2.States.StateMachines
 {
@@ -17,7 +19,7 @@ namespace GameDesign_2.States.StateMachines
     /// - AgroState.            End: 40%            Balance: 1 : 1
     /// - RegularState.         End: 55%            Balance: 4 : 1
     /// - EnrageState.          End: 10 seconds     Balance: 0 : 1
-    /// - Tough RegularState.   End: 60%            Balance: 3 : 1
+    /// - Tough RegularState.   End: 65%            Balance: 3 : 1
     /// - AgroState.            End: 75%            Balance: 1 : 1
     /// - EnrageState.          End: 10 seconds     Balance: 0 : 1
     /// - Tough RegularState.   End: 90%            Balance: 2 : 1
@@ -25,15 +27,77 @@ namespace GameDesign_2.States.StateMachines
     /// </summary>
     public class BossStateMachine : StateMachine
     {
+        //This number represents a state above.
+        private int stateNumber;
+
         public BossStateMachine(GameplayScreen screen)
             : base(screen)
         {
-
+            stateNumber = 0;
         }
 
         public override void Proceed(GameState caller)
         {
-            
+            //Clear the score stacks.
+            Screen.Player.ScoreBar.ClearStacks();
+
+            switch (stateNumber)
+            {
+                case 0: //To RegularState till 15%.
+                    stateNumber++;
+                    PushState(new RegularState(this, false, 15, 55));
+                    Spawner.GetInstance().FriendliesPerEnemies = 3;
+                    break;
+                case 1: //To tough RegularState till 30%.
+                    stateNumber++;
+                    PushState(new RegularState(this, true, 30));
+                    Spawner.GetInstance().FriendliesPerEnemies = 5;
+                    break;
+                case 2: //To AgroState till 40%. Repeat if below 20%.
+                    stateNumber++;
+                    PushState(new AgroState(this, 30));
+                    break;
+                case 3: //RegularState again till 55%.
+                    //Release the AgroState and the tough RegularState.
+                    PopState();
+                    stateNumber++;
+                    Spawner.GetInstance().FriendliesPerEnemies = 4;
+                    break;
+                case 4: //EnrageState.
+                    stateNumber++;
+                    PopState();
+                    PushState(new EnrageState(this));
+                    break;
+                case 5: //Tough RegularState till 65%.
+                    stateNumber++;
+                    PushState(new RegularState(this, true, 65, 90));
+                    Spawner.GetInstance().FriendliesPerEnemies = 3;
+                    break;
+                case 6: //AgroState till 75%. Repeat if below 55%.
+                    stateNumber++;
+                    PushState(new AgroState(this, 65));
+                    break;
+                case 7: //EnrageState.
+                    stateNumber++;
+                    PushState(new EnrageState(this));
+                    break;
+                case 8: //Tough RegularState till 90%.
+                    stateNumber++;
+                    Spawner.GetInstance().FriendliesPerEnemies = 2;
+                    break;
+                case 9: //AgroState till 100%. Repeat if below 80%.
+                    stateNumber++;
+                    PushState(new AgroState(this, 90));
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+
+            base.Update(gameTime);
         }
     }
 }
