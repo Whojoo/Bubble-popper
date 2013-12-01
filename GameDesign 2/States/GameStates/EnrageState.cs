aeadcf5e39c0b61eb56054f4dfc2554b3ef538c8
Robin de Gier <de.gier.robin@gmail.com>
2013-12-01 19:56:31 +0100

    Added 2 new States..
    TransitionState to transition between 2 StateMachines.
    EnrageState which doesnt do anything yet. It will spawn only enemies and all of them will target the player. After 10 seconds the State will clear. Multiplier is forced to x2 during Enrage.

---
+++ GameDesign 2/States/GameStates/EnrageState.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GameDesign_2.States.GameStates
+{
+    public class EnrageState : GameState
+    {
+        public EnrageState(StateMachine parent)
+            : base(parent)
+        {
+
+        }
+
+        public override void Activate()
+        {
+            base.Activate();
+        }
+    }
+}
